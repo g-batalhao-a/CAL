4e13b43cfde3f459dbd2905b28c53df49eb28668
Goncalo Alves <al.darkseidd.2010@gmail.com>
2020-04-14 15:59:41 +0100

    Finished TP_5

--- cal_fp05_CLion/Tests/Graph.h
+++ cal_fp05_CLion/Tests/Graph.h
@@ -101,15 +101,16 @@ template <class T>
 class Graph {
 	vector<Vertex<T> *> vertexSet;    // vertex set
 
-    vector<vector<double>> dist;
-    vector<vector<Vertex<T>*>> pred;
+    double ** W = nullptr;
+    int ** P = nullptr;
+    int findVertexIdx(const T &in) const;
 public:
 	Vertex<T> *findVertex(const T &in) const;
 	bool addVertex(const T &in);
 	bool addEdge(const T &sourc, const T &dest, double w);
 	int getNumVertex() const;
 	vector<Vertex<T> *> getVertexSet() const;
-
+    ~Graph();
 
 	// Fp05 - single source
 	void unweightedShortestPath(const T &s);    //DONE...
@@ -276,13 +277,52 @@ vector<T> Graph<T>::getPathTo(const T &dest) const{
     return res;
 }
 
+/** Finds the index of the vertex with a given content.*/
+template<class T>
+int Graph<T>::findVertexIdx(const T &in) const{
+    for(unsigned i = 0; i < vertexSet.size(); i++)
+        if(vertexSet[i]->info == in)
+            return i;
+        return-1;
+}
 
+template<class T>
+void deleteMatrix(T **m, int n) {
+    if(m != nullptr) {
+        for(int i = 0; i < n; i++)
+            if(m[i] != nullptr)
+                delete[] m[i];
+            delete[] m;
+    }
+}
+template<class T>
+Graph<T>::~Graph() {
+    deleteMatrix(W, vertexSet.size());
+    deleteMatrix(P, vertexSet.size());
+}
 
 /**************** All Pairs Shortest Path  ***************/
 
 template<class T>
 void Graph<T>::floydWarshallShortestPath() {
     // TODO
+    unsigned n = vertexSet.size();
+    deleteMatrix(W, n);
+    deleteMatrix(P, n);
+    W = new double*[n];
+    P = new int*[n];
+    for(unsigned i = 0; i < n; i++) {
+        W[i] = new double[n];
+        P[i] = new int[n];
+        for(unsigned j = 0; j < n; j++) {
+            W[i][j] = i == j? 0 : INF;
+            P[i][j] = -1;}
+        for(auto e : vertexSet[i]->adj) {
+            int j = findVertexIdx(e.dest->info);
+            W[i][j]  = e.weight;
+            P[i][j]  = i;}
+    }
+    /*
     dist.clear();
     dist = vector<vector<double>>(vertexSet.size(), vector<double>(vertexSet.size(), INT64_MAX));
     pred.clear();
@@ -315,7 +355,7 @@ void Graph<T>::floydWarshallShortestPath() {
             cout << x << "\t";
         }
         cout << endl;
-    }*/
+    }
 
     for (int k = 0; k < vertexSet.size(); k++) {
         int i = 0, j = 0;
@@ -347,13 +387,22 @@ void Graph<T>::floydWarshallShortestPath() {
                 cout << "NULL\t";
         }
         cout << endl;
-    }
+    }*/
 }
 
 template<class T>
 vector<T> Graph<T>::getfloydWarshallPath(const T &orig, const T &dest) const{
     vector<T> res;
     // TODO
+    int i = findVertexIdx(orig);
+    int j = findVertexIdx(dest);
+    if(i == -1 || j == -1 || W[i][j] == INF) // missing or disconnected
+        return res;
+    for( ; j != -1; j = P[i][j])
+        res.push_back(vertexSet[j]->info);
+    reverse(res.begin(), res.end());
+    return res;
+    /*
     int srcIndex, destIndex, i = 0;
 
     for (auto x : vertexSet) {
@@ -378,6 +427,7 @@ vector<T> Graph<T>::getfloydWarshallPath(const T &orig, const T &dest) const{
     res.push_back(dest);
 
     return res;
+     */
 }
 
 
