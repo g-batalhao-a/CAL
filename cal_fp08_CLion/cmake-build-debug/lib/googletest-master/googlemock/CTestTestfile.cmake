7b35fa6c03aa1c455939bbef844e84a1e8423970
g-batalhao-a <al.darkseidd.2010@gmail.com>
2020-04-28 16:31:51 +0100

    b

--- cal_fp08_CLion/cmake-build-debug/lib/googletest-master/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/GoncaloAlves/Desktop/FEUP/2_Ano/CAL/cal_fp08_CLion/lib/googletest-master/googlemock
-# Build directory: C:/Users/GoncaloAlves/Desktop/FEUP/2_Ano/CAL/cal_fp08_CLion/cmake-build-debug/lib/googletest-master/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
